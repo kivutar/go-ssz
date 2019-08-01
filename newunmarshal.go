0614d3bb8abcc7025265125c616beb2fee7a8e92
Raul Jordan <raul@prysmaticlabs.com>
2019-08-01 11:23:05 -0500

    include type as parameter

--- newunmarshal.go
+++ newunmarshal.go
@@ -31,32 +31,31 @@ func NewUnmarshal(input []byte, val interface{}) error {
 	if rval.IsNil() {
 		return errors.New("cannot output to pointer of nil value")
 	}
-	if _, err := newMakeUnmarshaler(input, rval.Elem(), 0); err != nil {
+	if _, err := newMakeUnmarshaler(input, rval.Elem(), rval.Elem().Type(), 0); err != nil {
 		return fmt.Errorf("could not unmarshal input into type: %v, %v", rval.Elem().Type(), err)
 	}
 	return nil
 }
 
-func newMakeUnmarshaler(input []byte, val reflect.Value, startOffset uint64) (uint64, error) {
-	typ := val.Type()
+func newMakeUnmarshaler(input []byte, val reflect.Value, typ reflect.Type, startOffset uint64) (uint64, error) {
 	kind := typ.Kind()
 	switch {
-	case kind == reflect.Bool:
-		return unmarshalBool(input, val, startOffset)
-	case kind == reflect.Uint8:
-		return unmarshalUint8(input, val, startOffset)
-	case kind == reflect.Uint16:
-		return unmarshalUint16(input, val, startOffset)
-	case kind == reflect.Uint32:
-		return unmarshalUint32(input, val, startOffset)
-	case kind == reflect.Int32:
-		return unmarshalUint32(input, val, startOffset)
-	case kind == reflect.Uint64:
-		return unmarshalUint64(input, val, startOffset)
-	//case kind == reflect.Slice && typ.Elem().Kind() == reflect.Uint8:
-	//	return makeByteSliceUnmarshaler()
-	//case kind == reflect.Array && typ.Elem().Kind() == reflect.Uint8:
-	//	return makeBasicArrayUnmarshaler(typ)
+	//case kind == reflect.Bool:
+	//	return unmarshalBool(input, val, typ, startOffset)
+	//case kind == reflect.Uint8:
+	//	return unmarshalUint8(input, val, typ, startOffset)
+	//case kind == reflect.Uint16:
+	//	return unmarshalUint16(input, val, typ, startOffset)
+	//case kind == reflect.Uint32:
+	//	return unmarshalUint32(input, val, typ, startOffset)
+	//case kind == reflect.Int32:
+	//	return unmarshalUint32(input, val, typ, startOffset)
+	//case kind == reflect.Uint64:
+	//	return unmarshalUint64(input, val, typ, startOffset)
+	case kind == reflect.Slice && typ.Elem().Kind() == reflect.Uint8:
+		return newByteSliceUnmarshaler(input, val, typ, startOffset)
+	case kind == reflect.Array && typ.Elem().Kind() == reflect.Uint8:
+		return newBasicArrayUnmarshaler(input, val, typ, startOffset)
 	//case kind == reflect.Slice && isBasicTypeArray(typ.Elem(), typ.Elem().Kind()):
 	//	return makeBasicSliceUnmarshaler(typ)
 	//case kind == reflect.Slice && isBasicType(typ.Elem().Kind()):
@@ -76,4 +75,100 @@ func newMakeUnmarshaler(input []byte, val reflect.Value, startOffset uint64) (ui
 	default:
 		return 0, fmt.Errorf("type %v is not deserializable", typ)
 	}
+}
+
+func newByteSliceUnmarshaler(input []byte, val reflect.Value, typ reflect.Type, startOffset uint64) (uint64, error) {
+	offset := startOffset + uint64(len(input))
+	val.SetBytes(input[startOffset:offset])
+	return offset, nil
+}
+
+func newBasicArrayUnmarshaler(input []byte, val reflect.Value, typ reflect.Type, startOffset uint64) (uint64, error) {
+	i := 0
+	index := startOffset
+	size := val.Len()
+	var err error
+	for i < size {
+		if val.Index(i).Kind() == reflect.Ptr {
+			instantiateConcreteTypeForElement(val.Index(i), typ.Elem().Elem())
+		}
+		index, err = newMakeUnmarshaler(input, val.Index(i), val.Index(i).Type(), index)
+		if err != nil {
+			return 0, fmt.Errorf("failed to unmarshal element of array: %v", err)
+		}
+		i++
+	}
+	return index, nil
+}
+
+func newBasicSliceUmmarshaler(input []byte, val reflect.Value, typ reflect.Type, startOffset uint64) (uint64, error) {
+	if len(input) == 0 {
+		newVal := reflect.MakeSlice(val.Type(), 0, 0)
+		val.Set(newVal)
+		return 0, nil
+	}
+	// If there are struct tags that specify a different type, we handle accordingly.
+	if val.Type() != typ {
+		sizes := []uint64{1}
+		innerElement := typ.Elem()
+		for {
+			if innerElement.Kind() == reflect.Slice {
+				sizes = append(sizes, 0)
+				innerElement = innerElement.Elem()
+			} else if innerElement.Kind() == reflect.Array {
+				sizes = append(sizes, uint64(innerElement.Len()))
+				innerElement = innerElement.Elem()
+			} else {
+				break
+			}
+		}
+		// If the item is a slice, we grow it accordingly based on the size tags.
+		result := growSliceFromSizeTags(val, sizes)
+		reflect.Copy(result, val)
+		val.Set(result)
+	} else {
+		growConcreteSliceType(val, val.Type(), 1)
+	}
+
+	var err error
+	index := startOffset
+	// TODO: USE Typ.
+	index, err = newMakeUnmarshaler(input, val.Index(0), val.Index(0).Type(), index)
+	if err != nil {
+		return 0, fmt.Errorf("failed to unmarshal element of slice: %v", err)
+	}
+
+	elementSize := index - startOffset
+	endOffset := uint64(len(input)) / elementSize
+	if val.Type() != typ {
+		sizes := []uint64{endOffset}
+		innerElement := typ.Elem()
+		for {
+			if innerElement.Kind() == reflect.Slice {
+				sizes = append(sizes, 0)
+				innerElement = innerElement.Elem()
+			} else if innerElement.Kind() == reflect.Array {
+				sizes = append(sizes, uint64(innerElement.Len()))
+				innerElement = innerElement.Elem()
+			} else {
+				break
+			}
+		}
+		// If the item is a slice, we grow it accordingly based on the size tags.
+		result := growSliceFromSizeTags(val, sizes)
+		reflect.Copy(result, val)
+		val.Set(result)
+	}
+	i := uint64(1)
+	for i < endOffset {
+		if val.Type() == typ {
+			growConcreteSliceType(val, val.Type(), int(i)+1)
+		}
+		index, err = newMakeUnmarshaler(input, val.Index(int(i)), val.Index(int(i)).Type(), index)
+		if err != nil {
+			return 0, fmt.Errorf("failed to unmarshal element of slice: %v", err)
+		}
+		i++
+	}
+	return index, nil
 }
